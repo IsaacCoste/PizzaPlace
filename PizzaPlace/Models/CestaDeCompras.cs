b1307551421a9b3d67e0e7d154585fc1d0cf5635
Isaac Patricio Coste Burgos <isaac_coste@ucne.edu.do>
2024-02-04 01:11:02 -0400

    Cree la clase de CestaDeCompras la cual tiene una lista y varios metodos dentro

---
+++ PizzaPlace/Models/CestaDeCompras.cs
@@ -0,0 +1,11 @@
+﻿namespace PizzaPlace.Models
+{
+    public class CestaDeCompras
+    {
+        public Clientes Clientes { get; set; } = new Clientes();
+        public List<int> Ordenes { get; set; } = new List<int>();
+        public bool HaPagado { get; set; }
+        public void Añadir(int pizzaId) => Ordenes.Add(pizzaId);
+        public void EliminarEn(int posicion) => Ordenes.RemoveAt(posicion);
+    }
+}
