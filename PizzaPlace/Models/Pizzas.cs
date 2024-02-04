24f6a0dbe52062d71e00d65a3018b1b4ccc1315b
Isaac Patricio Coste Burgos <isaac_coste@ucne.edu.do>
2024-02-04 01:06:04 -0400

    Cree la clase pizzas la cual tiene un constructor entre otras cosas

---
+++ PizzaPlace/Models/Pizzas.cs
@@ -0,0 +1,19 @@
+﻿using System.Xml.Linq;
+
+namespace PizzaPlace.Models
+{
+    public class Pizzas
+    {
+        public Pizzas(int id, string nombre, decimal precio, Picantes picante)
+        {
+            this.Id = id;
+            this.Nombre = nombre;
+            this.Precio = precio;
+            this.Picante = picante;
+        }
+        public int Id { get; }
+        public string Nombre { get; }
+        public decimal Precio { get; }
+        public Picantes Picante { get; }
+    }
+}
