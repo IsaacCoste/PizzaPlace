76c6e8feaa937a97acbdfad1a720b6798f3efac0
Isaac Patricio Coste Burgos <isaac_coste@ucne.edu.do>
2024-02-04 01:21:47 -0400

    Cree esta clase que almacena el estado de un sistema de pedidos de pizzas, incluye el menú entre otras cosa.

---
+++ PizzaPlace/Models/Estados.cs
@@ -0,0 +1,11 @@
+﻿namespace PizzaPlace.Models
+{
+    public class Estados
+    {
+        public Menu Menu { get; set; } = new Menu();
+        public CestaDeCompras Cesta { get; set; } = new CestaDeCompras();
+        public InterfazDeUsuarios InterfazUsuario { get; set; } = new InterfazDeUsuarios();
+        public decimal precioTotal => Cesta.Ordenes.Sum(id => Menu.GetPizza(id)!.Precio);
+        
+    }
+}
