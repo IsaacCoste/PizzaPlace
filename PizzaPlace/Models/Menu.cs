354424085da9f31d61fff974902f221e1dd41399
Isaac Patricio Coste Burgos <isaac_coste@ucne.edu.do>
2024-02-04 01:26:50 -0400

    Cree la clase ExtenDepuracion que convierte objetos en el codigo en formato Json

---
+++ PizzaPlace/Models/Menu.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace PizzaPlace.Models
+{
+    public class Menu
+    {
+        public List<Pizzas> Pizzas { get; set; } = new List<Pizzas>();
+        public void Add(Pizzas pizza) => Pizzas.Add(pizza);
+        public Pizzas? GetPizza(int id) => Pizzas.SingleOrDefault(pizza => pizza.Id == id);
+    }
+}
