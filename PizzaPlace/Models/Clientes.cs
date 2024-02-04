88de18ecbb398eb51eeb6603f0a996b6fd5aaf09
Isaac Patricio Coste Burgos <isaac_coste@ucne.edu.do>
2024-02-04 01:02:34 -0400

    Cree el modelo de clientes

---
+++ PizzaPlace/Models/Clientes.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PizzaPlace.Models
+{
+    public class Clientes
+    {
+        public int Id { get; set; }
+        [Required(ErrorMessage = "Por favor, proporcione su nombre")]
+        public string Nombre { get; set; } = default!;
+        [Required(ErrorMessage = "Por favor, proporcione la calle con su número de casa")]
+        public string Calle { get; set; } = default!;
+        [Required(ErrorMessage = "Por favor, proporcione su ciudad")]
+        public string Ciudad { get; set; } = default!;
+    }
+}
